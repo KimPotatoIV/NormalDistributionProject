980cee16287d86ef1729c8712d3308e9b35895f2
KimPotatoIV <jossceo@gmail.com>
2025-10-02 09:39:31 +0900

    Refactor distribution parameters and enhance histogram drawing
    Extracted distribution parameters (min, max, deviation) as constants for easier adjustment. Improved histogram visualization by changing bar colors and adding a yellow line connecting bar tops for better data representation.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,6 +5,10 @@ const SCREEN_SIZE: Vector2 = Vector2(1920.0, 1080.0)
 const DISTRIBUTION_COUNT: int = 100000
 const SECTION_COUNT: int = 100
 
+const MIN_VALUE: float = 0.0
+const MAX_VALUE: float = 100.0
+const DEVIATION: float = 15.0
+
 var distribution_array: Array = []
 
 func _ready() -> void:
@@ -14,7 +18,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		for i in range(DISTRIBUTION_COUNT):
-			var value: float = MathManager.get_ndn(0.0, 100.0, 15.0)
+			var value: float = MathManager.get_ndn(MIN_VALUE, MAX_VALUE, DEVIATION)
 			var value_to_int: int = round(value)
 			distribution_array[value_to_int - 1] += 1
 		
@@ -30,13 +34,27 @@ func _draw() -> void:
 			max_count = distribution_array[i]
 	
 	for i in range(SECTION_COUNT):
-		if max_count * max_height == 0:
-			break
-		
-		var bar_height = max_height * (float(distribution_array[i]) / float(max_count))
+		var bar_height = \
+			max_height * (float(distribution_array[i]) / float(max_count))
 		
 		draw_rect(Rect2(Vector2(i * bar_width, max_height - bar_height), \
-						Vector2(bar_width, bar_height)), Color.HOT_PINK)
+						Vector2(bar_width, bar_height)), Color.CORNFLOWER_BLUE)
 		
 		draw_rect(Rect2(Vector2(i * bar_width, max_height - bar_height), \
-						Vector2(bar_width, bar_height)), Color.MEDIUM_VIOLET_RED, false)
+						Vector2(bar_width, bar_height)), \
+						Color.BLUE, false)
+	
+	for i in range(SECTION_COUNT):
+		var bar_height = \
+			max_height * (float(distribution_array[i]) / float(max_count))
+		
+		var next_bar_height: float
+		if i == SECTION_COUNT - 1:
+			continue
+			
+		next_bar_height = \
+			max_height * (float(distribution_array[i + 1]) / float(max_count))
+		
+		draw_line(Vector2(i * bar_width + bar_width / 2, max_height - bar_height), \
+					Vector2((i + 1) * bar_width + bar_width / 2, \
+					max_height - next_bar_height), Color.YELLOW, 10.0)
